b933b781c1acbf82ea272cc3431248f924468a0f
Shubham Shete <shubham.shete@inadev.com>
2024-05-28 16:34:06 +0530

    4

--- main.tf
+++ main.tf
@@ -5,8 +5,11 @@ terraform {
   required_version = ">= 1.0.0"
 }
 
-# Documentation: https://registry.terraform.io/providers/hashicorp/local/latest/docs/resources/file
-resource "local_file" "changeme_local_file_hello" {
-  content  = "Hello terraform local!"
-  filename = "${path.module}/changeme_local_file_hello_${terraform.workspace}.txt"
+# # Documentation: https://registry.terraform.io/providers/hashicorp/local/latest/docs/resources/file
+# resource "local_file" "changeme_local_file_hello" {
+#   content  = "Hello terraform local!"
+#   filename = "${path.module}/changeme_local_file_hello_${terraform.workspace}.txt"
+# }
+
+resource "null_resource" "example" {
 }
